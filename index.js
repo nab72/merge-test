784ebb26578705c23271550458e6dcd0bcc11be3
nab72 <151378045+nab72@users.noreply.github.com>
2026-03-07 19:52:43 +0530

    Add button and script for date display on click

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 console.log("again Conflict created");
 console.log("Conflict resolved #1 test");
+<button id="myBtn">Click me</button>
+<p id="demo"></p>
+
+<script>
+  const btn = document.getElementById("myBtn");
+  btn.addEventListener("click", function() {
+    document.getElementById("demo").innerHTML = Date(); //
+  });
+</script>
