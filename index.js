1c8326c9de0fd55073b2cf0d7b30552926b91be5
nab72 <151378045+nab72@users.noreply.github.com>
2026-05-14 10:37:01 +0530

    Change variable x and update condition in index.js
    Updated variable x and modified the condition check.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("again Conflict created");
 console.log("Conflict resolved #1 test");
-let x = 5;
-if (x > 10) {
+let x = 50;
+if (x > 100) {
   console.log("x is greater than 10");
 } else {
   console.log("x is 10 or less");
